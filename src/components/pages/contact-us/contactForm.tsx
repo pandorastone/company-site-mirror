0e32572a2a0640c96c02e276ac1738bfcac1c4e7
Pandora Stone <pandorastoneit@gmail.com>
2023-05-19 13:11:56 -0400

    update function

--- src/components/pages/contact-us/contactForm.tsx
+++ src/components/pages/contact-us/contactForm.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import { send } from "@emailjs/browser"
+import { TiTick } from "react-icons/ti"
+import { RxCross2 } from "react-icons/rx"
 
 export default function ContactForm() {
   const [form, setForm] = useState({
@@ -7,6 +9,7 @@ export default function ContactForm() {
     email: "",
     message: ""
   })
+  const [isSent, setIsSent] = useState(0)
 
   function formSubmit(e: React.FormEvent) {
     e.preventDefault()
@@ -14,7 +17,12 @@ export default function ContactForm() {
       from_name: form.email,
       subject: form.subject,
       message: form.message
-    }, import.meta.env.VITE_EMAIL_PUBLIC_KEY)
+    }, import.meta.env.VITE_EMAIL_PUBLIC_KEY).then(
+       (res) => {
+        setIsSent(res.status)
+    }, (err) => {
+      setIsSent(err.status)
+    });
   }
 
   return (
@@ -26,6 +34,14 @@ export default function ContactForm() {
       <label htmlFor="message">Message</label>
       <textarea required name="message" className="border border-gray-300 rounded p-1" value={form.message} onChange={e => { setForm(prev => ({ ...prev, message: e.target.value })) }} />
       <button type="submit" className="bg-gray-600 p-2 rounded hover:bg-gray-500 text-white uppercase transition-[background-color]">send message</button>
+      {isSent === 200 ? (<div className="flex justify-start items-start gap-2">
+        <TiTick size={25}/>
+        <p>Your message has been sent! We will look into it as soon as possible.</p>
+      </div>) : null}
+      {isSent === 400 ? (<div className="flex justify-start items-start gap-2">
+        <RxCross2 size={25}/>
+        <p>Error! Message cannot be sent. Please try again later.</p>
+      </div>) : null}
     </form>
   )
 }
